5efb651b63b681fe12b8e29aa3b5828cd3144238
x1codias <xicodias1999@gmail.com>
2024-08-18 12:03:23 +0100

    feat(ui): functions to get and create the ingredients

---
+++ frontend/src/actions/ingredient.actions.ts
@@ -0,0 +1,54 @@
+import { Dispatch } from 'redux';
+import axios from 'axios';
+import {
+  GET_INGREDIENTS,
+  CREATE_INGREDIENT,
+} from './types';
+
+export const getIngredients =
+  (limit: number, offset: number) =>
+  async (dispatch: Dispatch) => {
+    try {
+      const userToken = localStorage.getItem('token');
+      const response = await axios.get(
+        import.meta.env.VITE_BACKEND_URL + '/ingredients',
+        {
+          params: {
+            limit: limit.toString(),
+            offset: offset.toString(),
+          },
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+          },
+        }
+      );
+      dispatch({
+        type: GET_INGREDIENTS,
+        payload: response.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+export const createIngredient =
+  (name: string) => async (dispatch: Dispatch) => {
+    try {
+      const userToken = localStorage.getItem('token');
+      const response = await axios.post(
+        import.meta.env.VITE_BACKEND_URL + '/ingredients',
+        { name },
+        {
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+          },
+        }
+      );
+      dispatch({
+        type: CREATE_INGREDIENT,
+        payload: response.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
