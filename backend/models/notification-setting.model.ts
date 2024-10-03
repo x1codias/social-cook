a5da7d36daff41f9eb62a0b71c8be08224783572
x1codias <xicodias1999@gmail.com>
2024-10-03 17:52:15 +0100

    feat(server): notification settings model

---
+++ backend/models/notification-setting.model.ts
@@ -0,0 +1,61 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../sequelize';
+import User from './user.model';
+
+export type NotificationSettingType = {
+  userId: number;
+  follow: boolean;
+  comment: boolean;
+  rating: boolean;
+  likeComment: boolean;
+};
+
+const NotificationSetting = sequelize.define<
+  Model<NotificationSettingType>
+>(
+  'NotificationSetting',
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User,
+        key: 'id',
+      },
+    },
+    follow: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    comment: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    rating: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    likeComment: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  },
+  {
+    tableName: 'notification_settings',
+    timestamps: true,
+  }
+);
+
+export default NotificationSetting;
