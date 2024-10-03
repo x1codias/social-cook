9ed82329b61145a9bad16f70dfb41dc364ae34db
x1codias <xicodias1999@gmail.com>
2024-10-03 17:41:40 +0100

    feat(server): blockage & settings model

---
+++ backend/models/blockage.model.ts
@@ -0,0 +1,37 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../sequelize';
+import User from './user.model';
+
+export type BlockageType = {
+  userId: number;
+  blockedId: number;
+  count?: number;
+};
+
+const Blockage = sequelize.define<Model<BlockageType>>(
+  'Blockage',
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User,
+        key: 'id',
+      },
+    },
+    blockedId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User,
+        key: 'id',
+      },
+    },
+  },
+  {
+    tableName: 'blockages',
+    timestamps: true,
+  }
+);
+
+export default Blockage;
---
+++ backend/models/settings.model.ts
@@ -0,0 +1,43 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../sequelize';
+import User from './user.model';
+
+export type SettingType = {
+  userId: number;
+  lang: string;
+  private: boolean;
+};
+
+const Setting = sequelize.define<Model<SettingType>>(
+  'Setting',
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User,
+        key: 'id',
+      },
+    },
+    lang: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    private: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  },
+  {
+    tableName: 'settings',
+    timestamps: true,
+  }
+);
+
+export default Setting;
